c719ca7748c2f68ac8b2291a86d66079452949e9
yannisfs <yannis.fus@gmail.com>
2025-02-06 16:35:46 +0100

    scroller test

--- docs/static/js/scroller.js
+++ docs/static/js/scroller.js
@@ -2,25 +2,25 @@ const scrollers = document.querySelectorAll(".scroller");
 
 // If a user hasn't opted in for recuded motion, then we add the animation
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-    addAnimation();
+  addAnimation();
 }
 
 function addAnimation() {
-    scrollers.forEach((scroller) => {
+  scrollers.forEach((scroller) => {
     // add data-animated="true" to every `.scroller` on the page
-        scroller.setAttribute("data-animated", true);
+    scroller.setAttribute("data-animated", true);
 
     // Make an array from the elements within `.scroller-inner`
-        const scrollerInner = scroller.querySelector(".scroller-inner");
-        const scrollerContent = Array.from(scrollerInner.children);
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
 
     // For each item in the array, clone it
     // add aria-hidden to it
     // add it into the `.scroller-inner`
-        scrollerContent.forEach((item) => {
-            const duplicatedItem = item.cloneNode(true);
-            duplicatedItem.setAttribute("aria-hidden", true);
-            scrollerInner.appendChild(duplicatedItem);
-        });
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
     });
+  });
 }
